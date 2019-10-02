2d1fd141c10ecbc1309056ed39dc159b7b5b4892
gissandrogama <gissandrogama@gmail.com>
2019-10-02 20:32:44 -0300

    conexão com o banco

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 const express = require ('express')
+const routes = require('./routes')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017/aircncapi', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 // req.query = acessar query params (para filtro)
 // req.params = acessar route params (para edição e delete)
 // req.body = acessar corpo da requisição (para criação e edição)
 
 app.use(express.json())
-
-app.post('/users', (req, res) => {
-    return res.json(req.body)
-})
+app.use(routes)
 
 app.listen(3333)
