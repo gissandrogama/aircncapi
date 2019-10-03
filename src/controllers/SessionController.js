79ea4012331081010d6cc3a2038677dc4b03597d
gissandrogama <gissandrogama@gmail.com>
2019-10-02 21:12:25 -0300

    session salvando no banco e Spot models

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,9 +1,19 @@
 //index, show, store, update, destroy
-
+const User = require('../models/User')
 
 module.exports = {
-    store(req, res){
-        return res.json({ message: 'Hello word' })
+    async store(req, res) {
+        const { email } = req.body
+
+        // variavel pode ser alterada
+        //busca por id 
+        let user = await User.findOne({ email })
+
+        if (!user) {
+            user = await User.create({ email })
+        }
+
+        return res.json(user)
     }
 
 }
