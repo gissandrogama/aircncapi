6589bd38412733460239abe90dabd623ae4abdd5
gissandrogama <gissandrogama@gmail.com>
2019-10-20 23:23:31 -0300

    enviado resposta para o mobile

--- src/controllers/ApprovalController.js
+++ src/controllers/ApprovalController.js
@@ -10,6 +10,15 @@ module.exports = {
 
         await booking.save()
 
+         //procurando o dono do spot
+         const bookingUserSocket = req.connectedUsers[booking.user]
+
+         //se existir uma conexão com esse id enviar uma mensagem
+         //passando todo o objeto booking
+         if(bookingUserSocket){
+             req.io.to(bookingUserSocket).emit('booking_response', booking)
+         }
+
         return res.json(booking)
     }
 }
